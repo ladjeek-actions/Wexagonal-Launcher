83a8c747fc72b735fe5138e1405e9e3e2c6e2512
Hajeekn <77675898+ladjeek-actions@users.noreply.github.com>
2022-03-26 17:44:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ let cachelist = [];
 const info = {
     version: "0.0.1-beta-13",
     dev: 0,
-    domain: "wexas.slqwq.cn",
+    domain: "dash.hesiy.cn",
     //endstatic: "static.wexa.top",
     //domain: "wexa.215213344.xyz",
     //endstatic: "static.215213344.xyz",
